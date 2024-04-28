11d62aa25174acfe531f0b3587840d10d00ebc6c
SBTheara <141052634+SBTheara@users.noreply.github.com>
2024-04-28 22:45:43 +0700

    second commit

--- src/main/java/com/project/springmvcdemo/controller/GreetingController.java
+++ src/main/java/com/project/springmvcdemo/controller/GreetingController.java
@@ -1,13 +1,15 @@
 package com.project.springmvcdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GreetingController {
 
     @RequestMapping("")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("name", "my heart will get out");
         return "index";
     }
 
